0c9fede6b9da76609189e1c67e2b9b200d0a08e4
Spiotwo <travkina08@gmail.com>
2021-12-09 03:21:27 +0300

    added project files

---
+++ src/main/java/com/github/spiotwo/AvgFlightTime/AvgFlightTimeApplication.java
@@ -0,0 +1,13 @@
+package com.github.spiotwo.AvgFlightTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvgFlightTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvgFlightTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/spiotwo/AvgFlightTime/AvgFlightTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spiotwo.AvgFlightTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvgFlightTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
